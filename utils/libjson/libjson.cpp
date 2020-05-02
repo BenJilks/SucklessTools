58e030cd9ad79a3117c8f21d637fb148a0392c7a
BenJilks <benjyjilks@gmail.com>
2020-05-02 23:21:09 +0100

    LibJson: Create a Json library

---
+++ utils/libjson/libjson.cpp
@@ -0,0 +1,256 @@
+#include "libjson.hpp"
+#include <sstream>
+
+#define TINYLEX_IMPLEMENT
+#include "lexer.hpp"
+
+using namespace Json;
+
+static std::shared_ptr<Value> parse_value(Lexer &lexer);
+
+static std::shared_ptr<Value> error(Lexer &lexer, std::string msg)
+{
+	lexer.error(msg);
+	std::cout << "Error: " << msg << "\n";
+	return nullptr;
+}
+
+static std::shared_ptr<Value> parse_object(Lexer &lexer)
+{
+	if (!lexer.consume(Token::TokenType::OpenObject))
+		return nullptr;
+	
+	auto object = std::make_shared<Object>();
+	while (!lexer.is_eof())
+	{
+		auto name = lexer.consume(Token::TokenType::String);
+		if (!name)
+			return error(lexer, "Expected a name");
+
+		if (!lexer.consume(Token::TokenType::Is))
+			return error(lexer, "Expected ':'");
+
+		auto value = parse_value(lexer);
+		if (!value)
+			return error(lexer, "Expected value");
+
+		auto name_str = lexer.read_string(*name);
+		name_str = name_str.substr(1, name_str.length() - 2);
+		object->set(name_str, value);
+
+		if (!lexer.consume(Token::TokenType::Next))
+			break;
+	}
+
+	if (!lexer.consume(Token::TokenType::CloseObject))
+		return error(lexer, "Expected '}'");
+
+	return object;
+}
+
+static std::shared_ptr<Value> parse_array(Lexer &lexer)
+{
+	if (!lexer.consume(Token::TokenType::OpenArray))
+		return nullptr;
+	
+	auto array = std::make_shared<Array>();
+	while (!lexer.is_eof())
+	{
+		auto value = parse_value(lexer);
+		if (!value)
+			return error(lexer, "Expected value");
+
+		array->append(value);
+
+		if (!lexer.consume(Token::TokenType::Next))
+			break;
+	}
+
+	if (!lexer.consume(Token::TokenType::CloseArray))
+		return error(lexer, "Expected ']'");
+	
+	return array;
+}
+
+static std::shared_ptr<Value> parse_string(Lexer &lexer)
+{
+	auto token = lexer.consume(Token::TokenType::String);
+	if (!token)
+		return error(lexer, "Expected string");
+
+	auto str = lexer.read_string(*token);
+	return std::make_shared<String>(str);
+}
+
+static std::shared_ptr<Value> parse_number(Lexer &lexer)
+{
+	auto token = lexer.consume(Token::TokenType::Number);
+	if (!token)
+		return error(lexer, "Expected number");
+
+	auto number = std::atof(lexer.read_string(*token).c_str());
+	return std::make_shared<Number>(number);
+}
+
+static std::shared_ptr<Value> parse_boolean(Lexer &lexer)
+{
+	auto token = lexer.consume(Token::TokenType::Boolean);
+	if (!token)
+		return error(lexer, "Expected boolean");
+
+	auto boolean = lexer.read_string(*token) == "true";
+	return std::make_shared<Boolean>(boolean);
+}
+
+static std::shared_ptr<Value> parse_value(Lexer &lexer)
+{
+	auto peek = lexer.peek();
+	if (!peek)
+		return nullptr;
+	
+	switch (peek->type)
+	{
+		case Token::TokenType::OpenObject: return parse_object(lexer);
+		case Token::TokenType::OpenArray: return parse_array(lexer);
+		case Token::TokenType::Number: return parse_number(lexer);
+		case Token::TokenType::String: return parse_string(lexer);
+		case Token::TokenType::Boolean: return parse_boolean(lexer);
+		default: return nullptr;
+	}
+}
+
+std::shared_ptr<Value> Json::parse(const std::string &file_path)
+{
+	Lexer lexer(file_path);
+	return parse_value(lexer);
+}
+
+static std::string print_indent(int indent)
+{
+	std::string out;
+	for (int i = 0; i < indent; i++)
+		out += "\t";
+	return out;
+}
+
+static std::string serialize(const Value &value, bool pretty_print, int indent = 0)
+{
+	if (value.is<String>())
+		return value.as<Json::String>()->get(); 
+	
+	if (value.is<Number>())
+	{
+		std::stringstream stream;
+		stream << std::noshowpoint << value.to_double(); 
+		return stream.str();
+	}
+	
+	if (value.is<Boolean>())
+		return value.as<Json::Boolean>()->get() ? "true" : "false";
+	
+	if (value.is<Object>())
+	{
+		std::string out = "{";
+
+		auto object = value.as<Json::Object>();
+		bool is_first = true;
+		int new_indent = indent + (pretty_print ? 1 : 0);
+		for (const auto &i : *object)
+		{
+			if (!is_first)
+				out += ", ";
+		
+			if (pretty_print)
+				out += "\n";
+
+			out += print_indent(new_indent);
+			out += "\"" + i.first + "\": ";
+
+			if (pretty_print && (i.second->is<Object>() || i.second->is<Array>()))
+				out += "\n" + print_indent(new_indent);
+
+			out += serialize(*i.second, pretty_print, 
+				new_indent);
+			
+			is_first = false;
+		}
+
+		if (pretty_print)
+			out += "\n" + print_indent(indent);
+
+		return out + "}";
+	}
+
+	if (value.is<Array>())
+	{
+		std::string out = "[";
+
+		auto array = value.as<Json::Array>();
+		bool is_first = true;
+		int new_indent = indent + (pretty_print ? 1 : 0);
+		for (const auto &item : *array)
+		{
+			if (!is_first)
+				out += ", ";
+			
+			if (pretty_print)
+				out += "\n";
+
+			out += print_indent(new_indent);
+			out += serialize(*item, pretty_print, new_indent);
+			
+			is_first = false;
+		}
+
+		if (pretty_print)
+			out += "\n" + print_indent(indent);
+
+		return out + "]";
+	}
+
+	return "";
+}
+
+std::string Value::to_string(bool pretty_print) const
+{
+	return serialize(*this, pretty_print);
+}
+
+float Value::to_float() const
+{
+	return static_cast<float>(as<Json::Number>()->get());
+}
+
+double Value::to_double() const
+{
+	return as<Json::Number>()->get();
+}
+
+int Value::to_int() const
+{
+	return static_cast<int>(as<Json::Number>()->get());
+}
+
+bool Value::to_bool() const
+{
+	return as<Json::Boolean>()->get();
+}
+
+std::ostream &Json::operator<< (std::ostream &out, const Value &value)
+{
+	out << value.to_string();
+	return out;
+}
+
+std::ostream &Json::operator<< (std::ostream &out, std::shared_ptr<Value> &value)
+{
+	if (!value)
+	{
+		out << "<Null value>";
+		return out;
+	}
+
+	out << *value;
+	return out;
+}
+
---
+++ utils/libjson/libjson.hpp
@@ -0,0 +1,233 @@
+#pragma once
+#include <memory>
+#include <map>
+#include <vector>
+#include <optional>
+
+namespace Json
+{
+
+	enum class ValueType
+	{
+		Object,
+		Array,
+		String,
+		Number,
+		Boolean,
+		Null
+	};
+
+	class Value
+	{
+	public:
+		Value() {}
+
+		template <typename T>
+		bool is() const { return this_type() == T::type(); }
+
+		template <typename T>
+		inline T *as() 
+		{
+			if (!is<T>())
+				return nullptr;
+
+			return static_cast<T*>(this);
+		}
+
+		template <typename T>
+		inline const T *as() const
+		{
+			if (!is<T>())
+				return nullptr;
+
+			return static_cast<const T*>(this);
+		}	
+
+		std::string to_string(bool pretty_print = false) const;
+		float to_float() const;
+		double to_double() const;
+		int to_int() const;
+		bool to_bool() const;
+
+	protected:
+		virtual ValueType this_type() const = 0;
+
+	};
+
+	class Null : public Value
+	{
+	public:
+		Null()
+			: Value()
+		{
+		}
+
+		inline static ValueType type() { return ValueType::Null; }
+
+	private:
+		virtual ValueType this_type() const override { return type(); }	
+
+	};
+
+	class Object : public Value
+	{
+	public:
+		Object()
+			: Value()
+		{
+		}
+
+		inline Value &get(const std::string &name) { return *data[name]; }
+		inline void set(const std::string &name, std::shared_ptr<Value> &value) { data[name] = value; }
+
+		template <typename T>
+		inline std::weak_ptr<T> add(const std::string &name) 
+		{ 
+			auto value = std::make_shared<T>();
+			data[name] = value;
+			return value; 
+		}
+
+		template <typename T>
+		inline T *get_as(const std::string &name) 
+		{ 
+			auto value = data[name];
+			if (!value)
+				return nullptr;
+			return value->as<T>();
+		}
+
+		auto begin() { return data.begin(); }
+		const auto begin() const { return data.begin(); }
+		auto end() { return data.end(); }
+		const auto end() const { return data.end(); }
+
+		inline static ValueType type() { return ValueType::Object; }
+
+	private:
+		virtual ValueType this_type() const override { return type(); }
+
+		std::map<std::string, std::shared_ptr<Value>> data;
+
+	};
+
+	
+
+	class Array : public Value
+	{
+	public:
+		Array()
+			: Value()
+		{
+		}
+
+		inline Value &get(int index) { return *data[index]; }
+		inline void set(int index, std::shared_ptr<Value> &value) 
+		{
+			if (data.size() <= index)
+				data.resize(index);
+			data[index] = value; 
+		}
+
+		inline void append(std::shared_ptr<Value> &value)
+		{
+			data.push_back(value);
+		}
+
+		template <typename T>
+		inline std::weak_ptr<T> append() 
+		{ 
+			auto value = std::make_shared<T>();
+			data.push_back(value);
+			return value;
+		}
+
+		template <typename T>
+		inline T *get_as(int index) { return data[index]->as<T>(); }
+
+		auto begin() { return data.begin(); }
+		const auto begin() const { return data.begin(); }
+		auto end() { return data.end(); }
+		const auto end() const { return data.end(); }
+
+		inline static ValueType type() { return ValueType::Array; }
+
+	private:
+		virtual ValueType this_type() const override { return type(); }
+
+		std::vector<std::shared_ptr<Json::Value>> data;
+
+	};
+
+	class String : public Value
+	{
+	public:
+		String(const std::string &str) 
+			: Value()
+			, data(str)
+		{
+		}
+
+		inline static ValueType type() { return ValueType::String; }
+
+		inline std::string &get() { return data; }
+		inline const std::string &get() const { return data; }
+		inline void set(const std::string &str) { data = str; }
+	
+	private:
+		virtual ValueType this_type() const override { return type(); }
+		
+		std::string data;
+
+	};
+
+	class Number : public Value
+	{
+	public:
+		Number(double number) 
+			: Value()
+			, data(number)
+		{
+		}
+
+		inline static ValueType type() { return ValueType::Number; }
+
+		inline double &get() { return data; }
+		inline const double &get() const { return data; }
+		inline void set(double d) { data = d; }
+	
+	private:
+		virtual ValueType this_type() const override { return type(); }
+		
+		double data;
+
+	};
+
+	class Boolean : public Value
+	{
+	public:
+		Boolean(bool boolean) 
+			: Value()
+			, data(boolean)
+		{
+		}
+
+		inline static ValueType type() { return ValueType::Boolean; }
+
+		inline bool &get() { return data; }
+		inline const bool &get() const { return data; }
+		inline void set(bool b) { data = b; }
+	
+	private:
+		virtual ValueType this_type() const override { return type(); }
+		
+		bool data;
+
+	};
+
+	std::shared_ptr<Value> parse(const std::string &file_path);
+	std::ostream &operator << (std::ostream &out, const Value &value);
+	std::ostream &operator << (std::ostream &out, std::shared_ptr<Value> &value);
+
+}
+
