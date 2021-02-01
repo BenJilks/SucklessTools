6ea267b6b2cec6b56054c7bf59b122cb53453fc8
BenJilks <benjyjilks@gmail.com>
2021-02-01 16:44:21 +0000

    Crond: Don't daemonize

--- utils/crond/src/main.rs
+++ utils/crond/src/main.rs
@@ -1,22 +1,19 @@
 #[macro_use]
 extern crate ini;
-extern crate daemonize;
 mod job;
 mod job_runner;
 use std::path::PathBuf;
 use std::time::Duration;
-use std::fs::File;
-use daemonize::Daemonize;
 
 fn run_background_process()
 {
     let config = ini!("/usr/etc/crond.conf");
     let cron_path = config["main"]["path"].clone().unwrap();
-    let verbose = config["main"]["verbose"].clone();
+    let verbose = config["main"].contains_key("verbose");
 
     loop
     {
-        if verbose.is_some() {
+        if verbose {
             println!("Doing loop");
         }
 
@@ -24,7 +21,7 @@ fn run_background_process()
         if result.is_err() {
             println!("Error: {}", result.unwrap_err());
         } else {
-            if verbose.is_some() {
+            if verbose {
                 println!("Done {} job(s)", result.unwrap());
             }
         }
@@ -35,18 +32,6 @@ fn run_background_process()
 
 fn main() 
 {
-    let stdout = File::create("/tmp/crond.out").unwrap();
-    let daemonize = Daemonize::new()
-        .pid_file("/tmp/crond.pid")
-        .chown_pid_file(true)
-        .stdout(stdout);
-
-    let result = daemonize.start();
-    if result.is_err() 
-    {
-        println!("Unable to daemonize: {}", result.unwrap_err());
-        return;
-    }
-
     run_background_process();
 }
+
